25a3b3e0db442f8f53cf4e4234fbc5b8db95ebe8
malt <malt@malt.dip.jp>
2013-09-29 20:40:48 +0900

    ajax

---
+++ public/js/malt.js
@@ -0,0 +1,33 @@
+function addPost(uri){
+	var val = $('#add [name=content]').val();
+	$.post(
+		uri,
+		{"content": val},
+		getData
+	);
+}
+
+function deletePost(uri, id){
+	$.post(
+		uri,
+		{"id": id},
+		getData
+	);
+}
+
+function editPost(uri, id){
+	var val = $('#edit_commit'+id+' [name=content]').val();
+	$.post(
+		uri,
+		{"id": id, "content": val},
+		getData
+	);
+}
+
+function getData(data, status){
+	if(status == "success"){
+		$('#task_list').html(data);
+	}else{
+		alert("Error:" + status);
+	}
+}
