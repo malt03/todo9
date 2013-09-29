136da61b7d6e62dcaa3edc821be6b38d483d0c7d
malt <malt@malt.dip.jp>
2013-09-29 20:39:48 +0900

    ajax

--- lib/todo9/Web.pm
+++ lib/todo9/Web.pm
@@ -5,7 +5,31 @@ use warnings;
 use utf8;
 use Kossy;
 use DBI;
+use Encode 'decode';
 use Data::Dumper;
+
+sub getTable{
+	my $database = 'DBI:mysql:DeNA';
+	my $username = 'dena';
+	my $password = 'shibuyahikarie';
+	my $dbh = DBI->connect($database, $username, $password);
+
+	my $sth = $dbh->prepare("SELECT * FROM todo9");
+	$sth->execute;
+	my $rows = $sth->fetchall_arrayref(+{});
+
+	$sth->finish;
+	$dbh->disconnect;
+
+	my $return_text = "<table border=\"1\">\n<tr><th>最終更新</th><th>内容</th><th></th><th></th></tr>";
+	foreach my $data (@$rows){
+		my $decode_content = decode('UTF-8', $data->{content});
+		$return_text .= "<tr><td>".$data->{updated_at}."</td><td>".$decode_content."</td>\n  <td>\n  <input type=\"button\" value=\"編集\" onClick=\"\$('#edit".$data->{id}."').get(0).style.display='block'\">\n  <div id=\"edit".$data->{id}."\" style=\"display:none\">\n	<form id=\"edit_commit".$data->{id}."\">\n	  <input name=\"id\" type=\"hidden\" value=\"\" />\n	  <input name=\"content\" type=\"textarea\" value=\"".$decode_content."\"><br/>\n	  <input type=\"button\" value=\"決定\" onClick=\"editPost('edit', ".$data->{id}.")\">\n	  <input type=\"button\" value=\"閉じる\" onClick=\"\$('#edit".$data->{id}."').get(0).style.display='none'\">\n	</form>\n  </div>\n  </td><td>\n	<input type=\"button\" value=\"削除\" onClick=\"deletePost('delete', ".$data->{id}.")\">\n  </td>\n</form>\n</td></tr>";
+	}
+	$return_text .= "</table>";
+
+	return $return_text;
+}
 
 filter 'set_title' => sub {
     my $app = shift;
@@ -71,13 +95,13 @@ post '/create' => sub {
 
 	my $content = $result->valid('content');
 	my $sth = $dbh->prepare("INSERT INTO todo9 (content) VALUES('$content')");
-
 	$sth->execute;
 	$sth->finish;
 	$dbh->disconnect;
 
-	return "OK!";
+	return getTable();
 };
+
 
 post '/delete' => sub {
 	my ($self, $c) = @_;
@@ -105,7 +129,7 @@ post '/delete' => sub {
 	$sth->finish;
 	$dbh->disconnect;
 
-	return "OK!";
+	return getTable;
 };
 
 post '/edit' => sub {
@@ -140,7 +164,7 @@ post '/edit' => sub {
 	$sth->finish;
 	$dbh->disconnect;
 
-	return "OK!";
+	return getTable;
 };
 
 1;
